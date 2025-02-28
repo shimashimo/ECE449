dc054fd6d7e40a676c93097e78aa8b11fc0ec178
Eric Yu <ericsentyu@gmail.com>
2025-02-27 20:35:24 -0800

    Program Counter and Ram implentation

--- 449_Project.srcs/sources_1/new/Program_Counter.vhd
+++ 449_Project.srcs/sources_1/new/Program_Counter.vhd
@@ -26,13 +26,36 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Program_Counter is
     Port ( 
-            Input : in STD_LOGIC;
-            Output : out STD_LOGIC);
+            brch_addr: in STD_LOGIC_VECTOR(15 downto 0);
+            brch_en: in STD_LOGIC;
+            rst: in STD_LOGIC;
+            clk: in STD_LOGIC;
+            stall: in STD_LOGIC;
+            PC : out STD_LOGIC_VECTOR(15 downto 0));
 end Program_Counter;
 
 architecture Behavioral of Program_Counter is
 
-begin
+signal prog_ctr: STD_LOGIC_VECTOR(15 downto 0);
 
+begin
+    process (clk) begin
+        if (rising_edge(clk)) then
+            if (rst = '1') then 
+                prog_ctr <= x"0000";
+            end if;
+
+            if (stall = 1) then
+                prog_ctr <= prog_ctr;
+            end if;
+
+            if (brch_en = 1) then
+                prog_ctr <= brch_addr;
+            else
+                prog_ctr <= std_logic_vector(signed(prog_ctr) + 2);
+            end if;
+    end process;
+            
+    PC <= prog_ctr;                
 
 end Behavioral;
